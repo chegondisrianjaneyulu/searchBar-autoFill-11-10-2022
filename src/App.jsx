9995c08e9948e24dcad9e9c674209d779975a91a
Sri <chegondisrianjaneyulu@gmail.com>
2022-11-10 12:05:38 +0530

    search-autofill

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import AutoFillSearchBar from './components/AutoFillSearchBar/AutoFillSearchBar'
+
+export default function App() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-slate-400'>
+      <AutoFillSearchBar/>
+    </div>
+  )
+}
